ebd02406d5002ea8e7565d3c27667c92347f7290
Mariana Meireles <mariana@psychonautgirl.space>
2020-04-06 22:46:55 +0200

    Add non void dependent tests

---
+++ test/test_db.cpp
@@ -0,0 +1,45 @@
+#ifndef TEST_DB_HPP
+#define TEST_DB_HPP
+
+#include "gtest/gtest.h"
+
+#include "xeus-sqlite/xeus_sqlite_interpreter.hpp"
+
+
+namespace xeus_sqlite
+{
+TEST(xeus_sqlite_interpreter, sanitize_string)
+{
+    std::string code = "\n\n Some inp\nut\n";
+    std::string sanitized_string;
+    sanitized_string = sanitize_string(code);
+    EXPECT_EQ(sanitized_string, "Some input");
+}
+
+TEST(xeus_sqlite_interpreter, tokenizer)
+{
+    std::string code = "%LOAD database.db rw";
+    std::vector<std::string> tokenized_code;
+    tokenized_code = tokenizer(code);
+    EXPECT_EQ(tokenized_code[2], "database.db");
+}
+
+TEST(xeus_sqlite_interpreter, is_magic)
+{
+    std::string code = "%LOAD database.db rw";
+    EXPECT_TRUE(is_magic(code));
+
+    std::string code = "SELECT *";
+    EXPECT_FALSE(is_magic(code));
+}
+
+TEST(xeus_sqlite_interpreter, create_destroy_db)
+{
+    std::vector<std::string> tokenized_code; 
+    tokenized_code[2] = ".";
+    tokenized_code[3] = "test.db";
+    tokenized_code = create_db(tokenized_code);
+}
+}
+
+#endif
