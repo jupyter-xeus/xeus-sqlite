63320be749914ee7fa20d5435b9eba8a4b54ed08
Mariana Meireles <mariana@psychonautgirl.space>
2020-06-09 16:43:38 +0200

    Fixes json bug now graph is rendered again

--- src/xeus_sqlite_interpreter.cpp
+++ src/xeus_sqlite_interpreter.cpp
@@ -306,21 +306,138 @@ void interpreter::parse_code(int execution_counter, std::vector<std::string>& to
 nl::json interpreter::create_vega_plot_bar(std::string& code)
 {
     nl::json pub_data = R"(
-        {
-          "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-          "width":800,
-          "height": 600,
-          "data": {
-            "values": [
-            ]
-          },
-          "mark": "bar",
-          "encoding": {
-            "x": {"field": "name", "type": "ordinal", "axis": {"labelAngle": 45}},
-            "y": {"field": "value", "type": "quantitative"}
-          }
-        }
-    )"_json;
+{
+   "$schema":"https://vega.github.io/schema/vega/v5.json",
+   "width":400,
+   "height":200,
+   "padding":5,
+   "data":[
+      {
+         "name":"table",
+         "values":[
+         ]
+      }
+   ],
+   "signals":[
+      {
+         "name":"tooltip",
+         "value":{
+
+         },
+         "on":[
+            {
+               "events":"rect:mouseover",
+               "update":"datum"
+            },
+            {
+               "events":"rect:mouseout",
+               "update":"{}"
+            }
+         ]
+      }
+   ],
+   "scales":[
+      {
+         "name":"xscale",
+         "type":"band",
+         "domain":{
+            "data":"table",
+            "field":"name"
+         },
+         "range":"width",
+         "padding":0.05,
+         "round":true
+      },
+      {
+         "name":"yscale",
+         "domain":{
+            "data":"table",
+            "field":"name"
+         },
+         "nice":true,
+         "range":"height"
+      }
+   ],
+   "axes":[
+      {
+         "orient":"bottom",
+         "scale":"xscale"
+      },
+      {
+         "orient":"left",
+         "scale":"yscale"
+      },
+   ],
+   "marks":[
+      {
+         "type":"rect",
+         "from":{
+            "data":"table"
+         },
+         "encode":{
+            "enter":{
+               "x":{
+                  "scale":"xscale",
+                  "field":"name"
+               },
+               "width":{
+                  "scale":"xscale",
+                  "band":1
+               },
+               "y":{
+                  "scale":"yscale",
+                  "field":"name"
+               },
+               "y2":{
+                  "scale":"yscale",
+                  "value":0
+               }
+            },
+            "update":{
+               "fill":{
+                  "value":"steelblue"
+               }
+            },
+            "hover":{
+               "fill":{
+                  "value":"red"
+               }
+            }
+         }
+      },
+      {
+         "type":"text",
+         "encode":{
+            "enter":{
+               "align":{
+                  "value":"center"
+               },
+               "baseline":{
+                  "value":"bottom"
+               },
+               "fill":{
+                  "value":"#333"
+               }
+            },
+            "update":{
+               "x":{
+                  "scale":"xscale",
+                  "signal":"tooltip.name",
+                  "band":0.5
+               },
+               "y":{
+                  "scale":"yscale",
+                  "signal":"tooltip.value",
+                  "offset":-2
+               },
+               "text":{
+                  "signal":"tooltip.value"
+               }
+            }
+         }
+      }
+   ]
+}    )"_json;
     SQLite::Statement query(*m_db, code);
 
     if (query.getColumnCount() != 0)
@@ -334,14 +451,11 @@ nl::json interpreter::create_vega_plot_bar(std::string& code)
         while (query.executeStep())
         {
             for (int column = 0; column < query.getColumnCount(); column++) {
-                std::string cell = query.getColumn(column);
-                if (column == 0)
+                // std::string cell = query.getColumn(column);
+                pub_data["data"][0]["values"][graph_bars_no]["name"] = query.getColumn(column);
+                if (graph_bars_no - 1 >= 0 && column + 1 < 2)
                 {
-                    pub_data["data"]["values"][graph_bars_no]["name"] = cell;
-                }
-                else if (column == 1)
-                {
-                    pub_data["data"]["values"][graph_bars_no - 1]["value"] = cell;
+                    pub_data["data"][0]["values"][graph_bars_no - 1]["value"] = query.getColumn(column + 1);
                 }
                 graph_bars_no++;
             }
