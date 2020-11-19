4e1a45a76a25b0286bbc467dba747771bf19966b
Mariana Meireles <mariana@psychonautgirl.space>
2020-11-19 18:31:05 +0200

    Change licenses to reflect xeus-sqlite contributors work and trim cmake

--- test/test_db.cpp
+++ test/test_db.cpp
@@ -1,3 +1,12 @@
+/***************************************************************************
+* Copyright (c) 2020, QuantStack and Xeus-SQLite contributors              *
+*                                                                          *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
 #ifndef TEST_DB_HPP
 #define TEST_DB_HPP
 
